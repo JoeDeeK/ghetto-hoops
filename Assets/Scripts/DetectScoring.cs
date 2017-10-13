31a29f4c8b4f8a3183ccd23adf767e9f6b90a161
Jody Kirton <jody@junctioncreative.ca>
2017-10-13 18:33:49 -0400

    Commiting all files to GitHub

--- Assets/Scripts/DetectScoring.cs
+++ Assets/Scripts/DetectScoring.cs
@@ -6,16 +6,6 @@ public class DetectScoring : MonoBehaviour {
 
     public int scorePerHit = 1;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     void OnCollisionEnter(Collision collision)
     {
         ScoreKeeper scorekeep = FindObjectOfType<ScoreKeeper>();
